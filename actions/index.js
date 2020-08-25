44644ff55bda2930cfdf9fda06c929535b9be71e
ariefirawan <ariefirawan@users.noreply.github.com>
2020-08-25 18:15:06 +0700

    useSWR to single data id

--- actions/index.js
+++ actions/index.js
@@ -12,6 +12,13 @@ const fetcher = (url) =>
   });
 
 export const useGetPosts = () => {
-  const {data, error, ...rest} = useSWR('/api/v1/posts', fetcher);
-  return {data, error, loading: !data && !error, ...rest}
+  const { data, error, ...rest } = useSWR('/api/v1/posts', fetcher);
+  return { data, error, loading: !data && !error, ...rest };
+};
+export const useGetPostByID = (id) => {
+  const { data, error, ...rest } = useSWR(
+    id ? `/api/v1/posts/${id}` : null,
+    fetcher
+  );
+  return { data, error, loading: !data && !error, ...rest };
 };
--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { withRouter } from 'next/router';
 import { useRouter } from 'next/router';
-import { useGetPosts } from '../../actions';
+import { useGetPostByID } from '../../actions';
 
 import BaseLayout from '../../components/BaseLayout';
 import BasePage from '../../components/BasePage';
 
 const Portfolio = () => {
   const router = useRouter();
-  const { posts: data, error, loading } = useGetPosts(
-    router.query.id ? `/api/v1/posts/${router.query.id}` : null
-  );
+  const { data, error, loading } = useGetPostByID(router.query.id);
   return (
     <BaseLayout>
       <BasePage>

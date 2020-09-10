835967a1fd562fc83c8b022e8a4215040182e91e
ariefirawan <ariefirawan@users.noreply.github.com>
2020-09-10 19:32:23 +0700

    create portfolio endpoint

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -2,9 +2,9 @@ import PortfolioApi from '../../../lib/api/portfolios';
 
 export default async function createPortfolio(req, res) {
   try {
-    const data = req.body;
-    await new PortfolioApi().createPortfolio(data);
-    return res.json({ message: successfully });
+    const portfolioData = req.body;
+    const json = await new PortfolioApi().createPortfolio(portfolioData);
+    return res.json(json.data);
   } catch (e) {
     return res.status(e.status || 400).end(e.message);
   }

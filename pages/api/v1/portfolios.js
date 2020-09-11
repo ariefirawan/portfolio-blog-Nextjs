8ecde3e4773bf0eca5c3f601471cba84711c6e2f
ariefirawan <ariefirawan@users.noreply.github.com>
2020-09-11 16:28:53 +0700

    handle token

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -6,7 +6,7 @@ export default async function createPortfolio(req, res) {
     const { accessToken } = await auth0.getSession(req);
     console.log(accessToken);
     const portfolioData = req.body;
-    const json = await new PortfolioApi().createPortfolio(portfolioData);
+    const json = await new PortfolioApi(accessToken).createPortfolio(portfolioData);
     return res.json(json.data);
   } catch (e) {
     return res.status(e.status || 400).end(e.message);

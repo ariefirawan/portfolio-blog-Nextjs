8c91ec9476472ba47a04bfebd45931972b112629
ariefirawan <ariefirawan@users.noreply.github.com>
2020-09-11 16:29:04 +0700

    Hook creator - Api handler

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,4 @@
-import useSWR from 'swr';
+import { useState } from 'react';
 
 export const fetcher = (url) =>
   fetch(url).then(async (res) => {
@@ -10,3 +10,38 @@ export const fetcher = (url) =>
       return result;
     }
   });
+
+export function useApiHandler(apicall) {
+  const [reqState, setReqState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  const handler = async (...data) => {
+    setReqState({
+      data: null,
+      loading: false,
+      error: null,
+    });
+
+    try {
+      const json = await apicall(...data);
+      setReqState({
+        data: json.data,
+        loading: false,
+        error: null,
+      });
+    } catch (e) {
+      const message =
+        (e.response && e.response.message) || 'ops..., something went wrong';
+      setReqState({
+        data: null,
+        loading: false,
+        error: message,
+      });
+    }
+  };
+
+  return [handler, { ...reqState }];
+}
--- pages/portfolio/new.js
+++ pages/portfolio/new.js
@@ -3,15 +3,16 @@ import BasePage from '../../components/BasePage';
 import withAuth from '../../hoc/WithAuth';
 import { Row, Col } from 'reactstrap';
 import PortfolioForm from '../../components/PortfolioForm';
-import { createPortfolio } from '../../actions/portfolios';
+import { useCreatePortfolio } from '../../actions/portfolios';
 
-const NewPortfolio = ({ user, loading }) => {
+const NewPortfolio = ({ user, loading: userLoading }) => {
+  const [createPortfolio, { data, loading, error }] = useCreatePortfolio();
   const _createPortfolio = (data) => {
     createPortfolio(data);
   };
 
   return (
-    <BaseLayout user={user} loading={loading}>
+    <BaseLayout user={user} loading={userLoading}>
       <BasePage header="Create Portfolio">
         <Row>
           <Col md="8">

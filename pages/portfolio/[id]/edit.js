ad232b6d901fafacbfbbcb2bba2ae1fba88595e3
arief irawan <ariefirawant@gmail.com>
2020-09-12 13:09:38 +0700

    update api client

--- pages/portfolio/[id]/edit.js
+++ pages/portfolio/[id]/edit.js
@@ -2,24 +2,30 @@ import BaseLayout from '../../../components/BaseLayout';
 import BasePage from '../../../components/BasePage';
 import withAuth from '../../../hoc/WithAuth';
 import { useRouter } from 'next/router';
-import { useGetPortfolio } from '../../../actions/portfolios';
+import {
+  useUpdatePortfolio,
+  useGetPortfolio,
+} from '../../../actions/portfolios';
 import PortfolioForm from '../../../components/PortfolioForm';
 import { Row, Col } from 'reactstrap';
 
 const PortfolioEdit = ({ user }) => {
   const router = useRouter();
-  const { data } = useGetPortfolio(router.query.id);
+  const [updatePortfolio, { data, error, loading }] = useUpdatePortfolio();
+  const { data: initialData } = useGetPortfolio(router.query.id);
+
+  const _updatePortfolio = (data) => {
+    updatePortfolio(router.query.id, data);
+  };
   return (
     <BaseLayout user={user} loading={false}>
       <BasePage header="portfolio Edit">
         <Row>
           <Col md="8">
-            {data && (
+            {initialData && (
               <PortfolioForm
-                onSubmit={(data) => {
-                  alert(JSON.stringify(data));
-                }}
-                initialData={data}
+                onSubmit={_updatePortfolio}
+                initialData={initialData}
               />
             )}
           </Col>

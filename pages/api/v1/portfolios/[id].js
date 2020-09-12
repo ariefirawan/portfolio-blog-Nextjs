d753e5074f990d5e3e084941a748cdc2c144cdd8
arief irawan <ariefirawant@gmail.com>
2020-09-12 17:27:37 +0700

    handle role delete access

--- pages/api/v1/portfolios/[id].js
+++ pages/api/v1/portfolios/[id].js
@@ -19,4 +19,14 @@ export default async function handlePortfolio(req, res) {
       return res.status(e.status || 422).json(e.response.data);
     }
   }
+
+  if (req.method === 'DELETE') {
+    try {
+      const { accessToken } = await auth0.getSession(req);
+      const json = await new PortfolioApi(accessToken).delete(req.query.id);
+      return res.json(json.data);
+    } catch (e) {
+      return res.status(e.status || 422).json(e.response.data);
+    }
+  }
 }
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import BaseLayout from '../../components/BaseLayout';
 import BasePage from '../../components/BasePage';
 import { useRouter } from 'next/router';
 import PortfolioApi from '../../lib/api/portfolios';
 import PortfilioCard from '../../components/PortfolioCard';
 import { getUser } from '../../actions/user';
+import { useDeletePortfolio } from '../../actions/portfolios';
 import { Row, Col, Button } from 'reactstrap';
 import { isAuthorized } from '../../utils/auth0';
 
-const Portfolios = ({ portfolios }) => {
-  const { data, loading } = getUser();
+const Portfolios = ({ portfolios: initialPortfolios }) => {
+  const [portfolios, setPorfolios] = useState(initialPortfolios);
+  const [deletePortfolio, { data, error }] = useDeletePortfolio();
+  const { data: dataU, loading } = getUser();
   const router = useRouter();
+
+  const _deletePortfolio = async (e, id) => {
+    e.stopPropagation();
+    const isConfirm = confirm('Are you sure want to delete?');
+    if (isConfirm) {
+      await deletePortfolio(id);
+      setPorfolios(portfolios.filter((p) => p._id !== id));
+    }
+  };
   //default return dari swr itu data
   return (
-    <BaseLayout user={data}>
+    <BaseLayout user={dataU} loading={loading}>
       <BasePage header="Portfolios" className="portfolio-page">
+        {error && <div className="alert alert-danger mt-2">{error}</div>}
         <Row>
           {portfolios.map((portfolio) => (
             <Col
@@ -24,7 +38,7 @@ const Portfolios = ({ portfolios }) => {
               md="4"
             >
               <PortfilioCard portfolio={portfolio}>
-                {data && isAuthorized(data, 'admin') && (
+                {dataU && (
                   <>
                     <Button
                       className="mr-2"
@@ -39,7 +53,12 @@ const Portfolios = ({ portfolios }) => {
                     >
                       Edit
                     </Button>
-                    <Button color="danger">Delete</Button>
+                    <Button
+                      color="danger"
+                      onClick={(e) => _deletePortfolio(e, portfolio._id)}
+                    >
+                      Delete
+                    </Button>
                   </>
                 )}
               </PortfilioCard>
